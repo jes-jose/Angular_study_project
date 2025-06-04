478dcae525dddabb43fb2862d80d7a4d011c9d13
jes-jose <jessjosethachedathu@gmail.com>
2025-06-04 14:24:56 +0530

    view recipe component design conmpleted

--- src/app/view-recipe/view-recipe.component.ts
+++ src/app/view-recipe/view-recipe.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-view-recipe',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './view-recipe.component.html',
   styleUrl: './view-recipe.component.css'
 })
