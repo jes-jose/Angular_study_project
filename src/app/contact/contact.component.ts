ea363b5827800be3a4dc2d264a1129060ea9a1ac
jes-jose <jessjosethachedathu@gmail.com>
2025-06-04 13:24:18 +0530

    home,about,contact,recipe pages completed

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
+    email:string = 'contact@cookpedia.com';
 }
